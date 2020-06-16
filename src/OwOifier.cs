faff8829c433eb7639242c8b6053a399ad210524
VAC Efron <46462862+VACEfron@users.noreply.github.com>
2020-06-16 19:55:18 +0200

    Now including furry vocabulary

--- src/OwOifier.cs
+++ src/OwOifier.cs
@@ -14,9 +14,9 @@ namespace OwOifier
             }
             if (OwOMode == OwOMode.Medium || OwOMode == OwOMode.High)
             {
-                Text = Regex.Replace(Text, @"n([aeiou])", "ny");
-                Text = Regex.Replace(Text, @"N([aeiou])", "Ny");
-                Text = Regex.Replace(Text, @"N([AEIOU])", "NY");
+                Text = Regex.Replace(Text, @"n([aeiou])", "ny$1");
+                Text = Regex.Replace(Text, @"N([aeiou])", "Ny$1");
+                Text = Regex.Replace(Text, @"N([AEIOU])", "NY$1");
                 Text = Regex.Replace(Text, @"z ", "z~ ");
             }
             if (OwOMode == OwOMode.High)
@@ -33,8 +33,62 @@ namespace OwOifier
                 Text = Regex.Replace(Text, "!", $" {faces[r.Next(faces.Length)]} ");
             }
 
-            Text = Text.Replace("nyzzwes", "nuzzles~");
-
+            Text = Text
+                .Replace("nyzzwes", "nuzzles~")
+                .Replace("ahh", "murr")
+                .Replace("awesome", "pawsome")
+                .Replace("awfuw", "pawful")
+                .Replace("bite", "nuzzles~")
+                .Replace("bite", "nom")
+                .Replace("buwge", "bulgy-wulgy")
+                .Replace("butthowe", "tailhole")
+                .Replace("cewebwity", "popufur")
+                .Replace("cheese", "sergal")
+                .Replace("chiwd", "cub")
+                .Replace("computew", "protogen")
+                .Replace("wobot", "protogen")
+                .Replace("cybowg", "protogen")
+                .Replace("cum", "cummy wummy~")
+                .Replace("disease", "pathOwOgen")
+                .Replace("dog", "good boy")
+                .Replace("dwagon", "derg")
+                .Replace("eat", "vore")
+                .Replace("fuck", "fluff")
+                .Replace("fathew", "daddy")
+                .Replace("foot", "footpaw")
+                .Replace("fow", "fur")
+                .Replace("hand", "paw")
+                .Replace("heww", "hecc")
+                .Replace("hyenya", "yeen")
+                .Replace("kiss", "lick")
+                .Replace("wmao", "hehe~")
+                .Replace("mouth", "maw")
+                .Replace("nyaughty", "knotty")
+                .Replace("nyot", "knot")
+                .Replace("pewfect", "purrfect")
+                .Replace("pewsona", "fursona")
+                .Replace("pewvewt", "furvert")
+                .Replace("pown", "yiff")
+                .Replace("shout", "awoo")
+                .Replace("swut", "fox")
+                .Replace("souwce", "saunce")
+                .Replace("stwaight", "gay")
+                .Replace("tawe", "tail")
+                .Replace("the", "teh")
+                .Replace("this", "dis")
+                .Replace("toe", "toe been")
+                .Replace("with", "wif")
+                .Replace("you", "chu")
+                .Replace("youw", "ur")
+                .Replace(",", "~")
+                .Replace(";", "~")
+                .Replace(":)", ":3")
+                .Replace(":O", "OwO")
+                .Replace(":o", "owo")
+                .Replace(":D", "UwU")
+                .Replace("XD", "X3")
+                .Replace("xD", "x3")
+                ;
             return Text;
         }
     }
